00c004eb4d1918406871a38539b6f1380542b3d7
Roger Reis <roger.gdreis@gmail.com>
2025-11-23 22:21:49 -0300

    datetime

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import yfinance as yf
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
+
 
 # criar as funções de carregamento de dados
     # Cotações
